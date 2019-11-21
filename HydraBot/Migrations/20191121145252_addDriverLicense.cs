7f960491502f6dc3c13e17067834deecad3bd8f8
Fooxboy <Fooxboy@yandex.ru>
2019-11-21 17:54:12 +0300

    Миграция

---
+++ HydraBot/Migrations/20191121145252_addDriverLicense.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HydraBot.Migrations
+{
+    public partial class addDriverLicense : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DriverLicense",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DriverLicense",
+                table: "Users");
+        }
+    }
+}
