90d1bd1302a9bc2303283f760980577d804e0e17
Fooxboy <Fooxboy@yandex.ru>
2019-11-25 13:18:05 +0300

    Добавил покупку бизнесов

---
+++ HydraBot/Commands/Store/BuyBusinessCommand.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using Fooxboy.NucleusBot;
+using Fooxboy.NucleusBot.Interfaces;
+using Fooxboy.NucleusBot.Models;
+using HydraBot.Models;
+
+namespace HydraBot.Commands.Store
+{
+    public class BuyBusinessCommand:INucleusCommand
+    {
+        public string Command => "buybusiness";
+        public string[] Aliases => new string[] {};
+        public void Execute(Message msg, IMessageSenderService sender, IBot bot)
+        {
+            var id = long.Parse(msg.Payload.Arguments[0]);
+            var text = string.Empty;
+            var kb = new KeyboardBuilder(bot);
+            var api = Main.Api;
+            var user = api.Users.GetUser(msg);
+            var price = 0;
+            if (id == 1)
+            {
+                price = 100;
+            }else if (id == 2)
+            {
+                price = 200;
+            }else if (id == 3) price = 200;
+            else if (id == 4) price = 50;
+            else if (id == 5) price = 199;
+            else if (id == 6) price = 192;
+            
+            
+            if (user.Money < price)
+            {
+                text = $"❌ У Вас недостаточно наличных, чтобы купить этот бизнес." +
+                       $"\n 💵 Ваш баланс: {user.Money} руб.";
+            }
+            else
+            {
+                api.Users.RemoveMoney(user.Id, price);
+                using (var db = new Database())
+                {
+                    var us = db.Users.Single(u => u.Id == user.Id);
+                    us.BusinessIds += $"{id},";
+                }
+
+                text = "✔ Вы купили бизнес!";
+            }
+
+            kb.AddButton("🛒 Назад в магазин", "store");
+            sender.Text(text, msg.ChatId, kb.Build());
+        }
+
+        public void Init(IBot bot, ILoggerService logger)
+        {
+        }
+    }
+}
---
+++ HydraBot/Services/BusinessService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Fooxboy.NucleusBot.Interfaces;
+using HydraBot.Models;
+
+namespace HydraBot.Services
+{
+    public class BusinessService :INucleusService
+    {
+        public string Name => "Buisines Service";
+        public bool IsRunning { get; set; }
+        public void Start(IBot bot, IBotSettings settings, List<IMessageSenderService> senders, ILoggerService logger)
+        {
+            IsRunning = true;
+            while (IsRunning)
+            {
+                Thread.Sleep(TimeSpan.FromHours(1));
+                using (var db = new Database())
+                {
+                    var users = db.Users.ToList();
+                    foreach (var user in users)
+                    {
+                        if (user.BusinessIds != "")
+                        {
+                            var ids = user.BusinessIds.Split(",");
+                            foreach (var id in ids)
+                            {
+                                if (id == "1") Main.Api.Users.AddMoney(user.Id, 100);
+                                if (id == "2") Main.Api.Users.AddMoney(user.Id, 100);
+                                if (id == "3") Main.Api.Users.AddMoney(user.Id, 100);
+                                if (id == "4") Main.Api.Users.AddMoney(user.Id, 100);
+                                if (id == "5") Main.Api.Users.AddMoney(user.Id, 100);
+                                if (id == "6") Main.Api.Users.AddMoney(user.Id, 100);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            IsRunning = false;
+        }
+    }
+}
