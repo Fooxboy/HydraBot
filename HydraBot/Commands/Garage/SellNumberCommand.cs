7a01a50cde14fb2364ef1a7b6e20c6a30c95a7db
Fooxboy <Fooxboy@yandex.ru>
2019-12-06 02:44:42 +0300

    Добавил заглушку для продажи номеров

---
+++ HydraBot/Commands/Garage/SellNumberCommand.cs
@@ -0,0 +1,19 @@
+﻿using Fooxboy.NucleusBot.Interfaces;
+using Fooxboy.NucleusBot.Models;
+
+namespace HydraBot.Commands.Garage
+{
+    public class SellNumberCommand:INucleusCommand
+    {
+        public string Command => "sellnumber";
+        public string[] Aliases => new string[0];
+        public void Execute(Message msg, IMessageSenderService sender, IBot bot)
+        {
+            sender.Text("❓ Продажа номеров появится в будущем.", msg.ChatId);
+        }
+
+        public void Init(IBot bot, ILoggerService logger)
+        {
+        }
+    }
+}
