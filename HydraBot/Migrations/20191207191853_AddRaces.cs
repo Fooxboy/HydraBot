325bd8682a58be126cb42937568d1c6c3ec4886b
Fooxboy <Fooxboy@yandex.ru>
2019-12-07 22:38:26 +0300

    Обновлена база данных

---
+++ HydraBot/Migrations/20191207191853_AddRaces.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HydraBot.Migrations
+{
+    public partial class AddRaces : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Race",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateTable(
+                name: "Races",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Creator = table.Column<long>(nullable: false),
+                    Enemy = table.Column<long>(nullable: false),
+                    IsRequest = table.Column<bool>(nullable: false),
+                    Winner = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Races", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Races");
+
+            migrationBuilder.DropColumn(
+                name: "Race",
+                table: "Users");
+        }
+    }
+}
