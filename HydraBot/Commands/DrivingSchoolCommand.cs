79300971b592c33c552e3e928a0b36ae121b4a67
Fooxboy <Fooxboy@yandex.ru>
2019-12-13 10:21:58 +0300

    Фикс автошколы

--- HydraBot/Commands/DrivingSchoolCommand.cs
+++ HydraBot/Commands/DrivingSchoolCommand.cs
@@ -21,12 +21,12 @@ namespace HydraBot.Commands
                 "\n ❓ Выберите категорию на клавиатуре ниже";
             var user = Main.Api.Users.GetUser(msg);
             var kb = new KeyboardBuilder(bot);
-            if(user.DriverLicense.Split(",").Any(s=> s == "A")) kb.AddButton("A (1 рубль)", "catA", new List<string> {"0", "0", "0" });
-            if (user.DriverLicense.Split(",").Any(s => s == "B")) kb.AddButton("B (1 рубль)", "catB", new List<string> {"0", "0", "0" });
-            if (user.DriverLicense.Split(",").Any(s => s == "A" || s== "B")) kb.AddLine();
-            if (user.DriverLicense.Split(",").Any(s => s == "C")) kb.AddButton("C (1 рубль)", "catC", new List<string> { "0", "0", "0" });
-            if (user.DriverLicense.Split(",").Any(s => s == "D")) kb.AddButton("D (1 рубль)", "catD", new List<string> { "0", "0", "0" });
-            if (user.DriverLicense.Split(",").Any(s => s == "C" || s == "D")) kb.AddLine();
+            if(!(user.DriverLicense.Split(",").Any(s=> s == "A"))) kb.AddButton("A (1 рубль)", "catA", new List<string> {"0", "0", "0" });
+            if (!(user.DriverLicense.Split(",").Any(s => s == "B"))) kb.AddButton("B (1 рубль)", "catB", new List<string> {"0", "0", "0" });
+            if (!(user.DriverLicense.Split(",").Any(s => s == "A" || s== "B"))) kb.AddLine();
+            if (!(user.DriverLicense.Split(",").Any(s => s == "C"))) kb.AddButton("C (1 рубль)", "catC", new List<string> { "0", "0", "0" });
+            if (!(user.DriverLicense.Split(",").Any(s => s == "D"))) kb.AddButton("D (1 рубль)", "catD", new List<string> { "0", "0", "0" });
+            if (!(user.DriverLicense.Split(",").Any(s => s == "C" || s == "D"))) kb.AddLine();
             kb.AddButton(ButtonsHelper.ToHomeButton());
             sender.Text(text, msg.ChatId, kb.Build());
         }
