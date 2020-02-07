39f5a6c2e05ffa261ef328d85c7a6ba854c253bc
Fooxboy <fooxboy@yandex.ru>
2020-02-07 16:56:22 +0300

    Обновил базу данных

---
+++ HydraBot/Migrations/20200207135519_addCasino.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HydraBot.Migrations
+{
+    public partial class addCasino : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Chips",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Chips",
+                table: "Users");
+        }
+    }
+}
