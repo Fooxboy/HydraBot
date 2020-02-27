c8b89b4d6b791656079a5bf648b0e22ff09348cd
Fooxboy <fooxboy@yandex.ru>
2020-02-27 20:47:42 +0300

    Обновление базы данных для поддержки сообщений

---
+++ HydraBot/Migrations/20200227174702_AddMessages.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HydraBot.Migrations
+{
+    public partial class AddMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MessagesInfo",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LastMessageUsers = table.Column<string>(nullable: true),
+                    LastMessageText = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessagesInfo", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MessagesInfo");
+        }
+    }
+}
